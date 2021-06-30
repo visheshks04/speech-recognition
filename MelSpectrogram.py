8c99c87893c2416a77b1ebd0014aa40b08af93f9
Vishesh Kumar Singh <47525494+visheshks04@users.noreply.github.com>
2021-06-30 12:12:35 +0530

    Add files via upload

---
+++ MelSpectrogram.py
@@ -0,0 +1,25 @@
+import librosa
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def get_mel_spect(path):
+    y, sr = librosa.load(path)
+    return librosa.feature.melspectrogram(y, sr)
+
+def insert_spect_col(df):
+    '''
+    Inserts another column in df with corresponding Mel Spectograms
+    '''
+    spects = pd.Series([])
+    for path in df['path']:
+        path = 'clips/' + path
+        spect = get_mel_spect(path)
+        spects.append(pd.Series([spect]))
+
+    new_df = pd.concat([df, spects], axis = 1)
+    new_cols = list(new_df.columns)
+    new_cols[-1] = 'mel'
+    new_df.columns = new_cols
+    return new_df
